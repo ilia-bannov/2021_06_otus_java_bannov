1da40a9efe69708009ac8bd0a8c291dd55b9a27c
Ilia Bannov <bannov.ilya@gmail.com>
2023-08-08 22:12:19 +0400

    add readme and use guava

--- hw01-gradle/src/main/java/ru/otus/App.java
+++ hw01-gradle/src/main/java/ru/otus/App.java
@@ -1,7 +1,12 @@
 package ru.otus;
 
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello Otus!");
+
+        System.out.println(Lists.reverse(List.of("Otus!", "Hello")));
     }
 }
