90234cd79f9900d07c7c13cf87eba7c5df9dec09
Ilia Bannov <bannov.ilya@gmail.com>
2023-08-27 00:24:02 +0400

    hw02-collections

---
+++ hw02-collections/src/main/java/ru/otus/Customer.java
@@ -0,0 +1,61 @@
+package ru.otus;
+
+import static java.util.Objects.hash;
+
+public class Customer {
+    private final long id;
+    private String name;
+    private long scores;
+
+    //todo: 1. в этом классе надо исправить ошибки
+
+    public Customer(long id, String name, long scores) {
+        this.id = id;
+        this.name = name;
+        this.scores = scores;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getScores() {
+        return scores;
+    }
+
+    public void setScores(long scores) {
+        this.scores = scores;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", scores=" + scores +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Customer customer = (Customer) o;
+
+        return id == customer.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return hash(id);
+    }
+}
